b930bc4a41cae417af4e42f099952153ae95e584
Eelis Holmstén <holmsteneelis@gmail.com>
2025-01-02 19:14:28 +0200

    added fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_greeting
+    # echo 'Hello'
+end
+
+export EDITOR=/opt/nvim-linux64/bin/nvim
+
+export PATH="$PATH:/opt/nvim-linux64/bin"
+export PATH="$PATH:/home/eelis/zig-linux-x86_64-0.14.0-dev.2050+4adf63aef/"
+export PATH="$PATH:/home/eelis/programming/thrid-party/"
+export PATH="$PATH:/usr/local/go/bin"
+export PATH="$PATH:/home/eelis/go/bin"
